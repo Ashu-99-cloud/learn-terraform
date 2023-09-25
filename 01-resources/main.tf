ff91a3abbd85a604a530871e1f6eb7df6ae3db30
Ashish Ujawane <ashish.ujawane@morningstar.com>
2023-09-25 21:51:51 +0530

    code for route 53. Adding record in route 53

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -7,3 +7,10 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_route53_record" "www" {
+  zone_id = 'Z0541078297KU8FMA9FR1'
+  name    = "test.ashudevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.web.private_ip]
+}
