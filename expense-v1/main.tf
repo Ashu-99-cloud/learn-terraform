babe77ee6e6830f1658cddef52ce50b428b51b44
Ashish Ujawane <ashish.ujawane@morningstar.com>
2023-09-25 22:32:13 +0530

    create frontend, backend and mysql server, added the security group and update the route 53 record

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -8,10 +8,46 @@ resource "aws_instance" "frontend" {
   }
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "frontend" {
   zone_id = "Z0541078297KU8FMA9FR1"
   name    = "frontend.ashudevops.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.web.private_ip]
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-09ee145e84c525325"]
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = "Z0541078297KU8FMA9FR1"
+  name    = "backend.ashudevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-09ee145e84c525325"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0541078297KU8FMA9FR1"
+  name    = "mysql.ashudevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
