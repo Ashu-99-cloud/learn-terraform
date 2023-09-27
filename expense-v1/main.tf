3042bf51c84dbf44cc028c625b0a0e9813523324
Ashish Ujawane <ashish.ujawane@morningstar.com>
2023-09-27 10:59:17 +0530

    adding provisioner and running the ansible commands for frontend, backend and mysql. So that all the servers will be created.

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -7,17 +7,7 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/infra-ansible
-git pull
-sleep 60
-ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
-EOF
-   }
 }
-
-
 
 
 resource "aws_route53_record" "frontend" {
@@ -28,6 +18,20 @@ resource "aws_route53_record" "frontend" {
   records = [aws_instance.frontend.private_ip]
 }
 
+resource "null_resource" "frontend" {
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/infra-ansible
+git pull
+sleep 60
+ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+EOF
+   }
+}
+
+
+
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
@@ -36,14 +40,6 @@ resource "aws_instance" "backend" {
   tags = {
     Name = "backend"
   }
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/infra-ansible
-git pull
-sleep 60
-ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
-EOF
-   }
 }
 
 resource "aws_route53_record" "backend" {
@@ -54,6 +50,19 @@ resource "aws_route53_record" "backend" {
   records = [aws_instance.backend.private_ip]
 }
 
+resource "null_resource" "backend" {
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/infra-ansible
+git pull
+sleep 60
+ansible-playbook -i ${aws_instance.backend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
+EOF
+   }
+}
+
+
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
@@ -62,15 +71,6 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql"
   }
-
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/infra-ansible
-git pull
-sleep 60
-ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
-EOF
-   }
 }
 
 resource "aws_route53_record" "mysql" {
@@ -79,4 +79,16 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.mysql.private_ip]
+}
+
+resource "null_resource" "mysql" {
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/infra-ansible
+git pull
+sleep 60
+ansible-playbook -i ${aws_instance.mysql.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
+EOF
+   }
 }
